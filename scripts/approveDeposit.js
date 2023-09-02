b4f5a7a56f35ce77f22f01a3431fdee7f2b5ac4f
codeWithNoob <136970849+codeWithNoob@users.noreply.github.com>
2023-09-02 11:33:56 +0530

    Update approveDeposit.js

--- scripts/approveDeposit.js
+++ scripts/approveDeposit.js
@@ -1,39 +1,69 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
-const fxRootContractABI = require("../fxRootContractABI.json");
-const tokenContractJSON = require("../artifacts/contracts/MetaToken.sol/MetaToken.json");
+// Import necessary packages and contracts
+const { ethers } = require("hardhat");
+const FXRootContractAbi = require('../artifacts/FXRootContractAbi.js');
+const FlyingCarAbi = require('../artifacts/contracts/flyingcar.sol/flyingcar.json');
+require('dotenv').config();
 
-const tokenAddress = ""; // place your erc20 contract address here
-const tokenABI = tokenContractJSON.abi;
-const fxERC20RootAddress = "0x3658ccFDE5e9629b0805EB06AaCFc42416850961";
-const walletAddress = ""; // place your public address for your wallet here
+// Function to transfer ERC721A tokens to the Ethereum FxChain network
+async function transferTokensToFxChain() {
+  try {
+    // Set up connections to network and wallet
+    const networkAddress = 'https://ethereum-goerli.publicnode.com';
+    const privateKey = process.env.PRIVATE_KEY;
+    const provider = new ethers.providers.JsonRpcProvider(networkAddress);
 
-async function main() {
+    // Create a wallet instance
+    const wallet = new ethers.Wallet(privateKey, provider);
 
-    const tokenContract = await hre.ethers.getContractAt(tokenABI, tokenAddress);
-    const fxContract = await hre.ethers.getContractAt(fxRootContractABI, fxERC20RootAddress);
+    // Get the signer instance
+    const [signer] = await ethers.getSigners();
 
-    const approveTx = await tokenContract.approve(fxERC20RootAddress, 500);
+    // Get ERC721A contract instance
+    const FlyingCar = await ethers.getContractFactory("flyingcar");
+    const nft = await FlyingCar.attach('0x0b79BD281eC4Ac1BA224Ff0A6C336A863063BDc8');
+
+    // Get FXRoot contract instance
+    const fxRootAddress = '0xF9bc4a80464E48369303196645e876c8C7D972de';
+    const fxRoot = await ethers.getContractAt(FXRootContractAbi, fxRootAddress);
+
+    // TokenIds to transfer
+    const tokenIds = [0, 1, 2, 3, 4];
+
+    // Approve the NFTs for transfer
+    const approveTx = await nft.connect(signer).setApprovalForAll(fxRootAddress, true);
     await approveTx.wait();
-
     console.log('Approval confirmed');
 
+    // Deposit the NFTs to the FXRoot contract
+    for (let i = 0; i < tokenIds.length; i++) {
+      const depositTx = await fxRoot.connect(signer).deposit(
+        nft.address,
+        wallet.address,
+        tokenIds[i],
+        '0x6566'
+      );
 
-    const depositTx = await fxContract.deposit(tokenAddress, walletAddress, 500, "0x6556");
-    await depositTx.wait();
+      // Wait for the deposit to be confirmed
+      await depositTx.wait();
+    }
 
-    console.log("Tokens deposited");
-  
+    console.log("Approved and deposited");
+
+    // Test balanceOf
+    const balance = await nft.balanceOf(wallet.address);
+
+    // Print the balance of the wallet
+    console.log("IndianNFT wallet balance", wallet.address, "is:", balance.toString());
+  } catch (error) {
+    console.error("Error transferring tokens:", error);
+    process.exit(1);
   }
-  
-  // We recommend this pattern to be able to use async/await everywhere
-  // and properly handle errors.
-  main().catch((error) => {
+}
+
+// Call the transfer function and handle any errors
+transferTokensToFxChain()
+  .then(() => process.exit(0))
+  .catch((error) => {
     console.error(error);
-    process.exitCode = 1;
+    process.exit(1);
   });
